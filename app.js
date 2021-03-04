fe0ff9693990a55296b5204506a0b01ff7bbacfc
arnone215 <tarnone21589@gmail.com>
2021-03-03 17:57:03 -0800

    using forLoop to DRY code

--- app.js
+++ app.js
@@ -16,4 +16,43 @@ new Movie ('Tom and Jerry', 'kids', 3, 'Jim Carey', 'movie night');
 new Movie ('Den of Thieves', 'action', 4, 'Gerard Butler', 'bank robbery');
 
 
+// let movieParent = document.getElementById('movielist');
+// let child = document.createElement('li');
+// let currentMovie = allMovies[0];
+// child.textContent = currentMovie.title;
+// movieParent.appendChild(child);
+
+// let childTwo = document.createElement('li');
+// currentMovie = allMovies[1];
+// childTwo.textContent = currentMovie.title;
+// movieParent.appendChild(childTwo);
+
+// let childThree = document.createElement('li');
+// currentMovie = allMovies[2];
+// childThree.textContent = currentMovie.title;
+// movieParent.appendChild(childThree);
+
+// BELOW IS THE WAY TO DRY IT OUT
+
+
+
+let movieParent = document.getElementById('movielist');
+for (let i = 0; i < allMovies.length; i++) {
+  let child = document.createElement('li');
+  let currentMovie = allMovies[i];
+  child.textContent = currentMovie.title;
+  movieParent.appendChild(child);
+}
+
+//Now you just delete everything under here that is commented out.
+
+// let childTwo = document.createElement('li');
+// currentMovie = allMovies[1];
+// childTwo.textContent = currentMovie.title;
+// movieParent.appendChild(childTwo);
+
+// let childThree = document.createElement('li');
+// currentMovie = allMovies[2];
+// childThree.textContent = currentMovie.title;
+// movieParent.appendChild(childThree);
 
